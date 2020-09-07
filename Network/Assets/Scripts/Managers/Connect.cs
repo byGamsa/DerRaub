030471cb662143605dd16eefe6f0880a2cdcbd6c
byGamsa <larsgerigk@gmx.de>
2020-09-07 22:12:17 +0200

    Add Project

---
+++ Network/Assets/Scripts/Managers/Connect.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connect : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting to Server...");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server!");
+        Debug.Log("Player: " + PhotonNetwork.LocalPlayer.NickName);
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected from Server! ( " + cause.ToString() + " )");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
